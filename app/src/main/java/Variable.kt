f8e21632ed1936ceeb0fbbc372a5845898c1feae
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2023-01-01 15:59:10 +0900

    val

---
+++ app/src/main/java/Variable.kt
@@ -0,0 +1,12 @@
+package com.jp_funda.playground
+
+fun main() {
+    val year: Int = 2023
+    val month = 1
+    val day: Int
+    day = 1
+
+    println(year)
+    println(month)
+    println(day)
+}
